891ac3dbf7205a90e661ae33dc8e4be81effc606
ebearden <elvin.bearden@gmail.com>
2014-06-19 17:22:42 -0500

    Added JDBCHelper.

---
+++ src/com/scrumware/jdbc/JDBCHelper.java
@@ -0,0 +1,33 @@
+package com.scrumware.jdbc;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+import javax.naming.Context;
+import javax.naming.InitialContext;
+import javax.naming.NamingException;
+import javax.sql.DataSource;
+
+public class JDBCHelper {
+	/**
+	 * Get a connection to the database
+	 * @return the connection.
+	 */
+	public static Connection getConnection() {
+		Connection con = null;
+		Context initCtx;
+		Context envCtx;
+		DataSource ds;
+		
+		try {
+			initCtx = new InitialContext();
+			envCtx = (Context) initCtx.lookup("java:comp/env");
+			ds = (DataSource) envCtx.lookup("jdbc/scrumwarestaging");
+			con = ds.getConnection();
+		} catch (NamingException | SQLException e1) {
+			e1.printStackTrace();
+		}
+		return con;
+	}
+}
