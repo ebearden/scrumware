2710ab4bf3a44cc0344bed3cf3a3060717256762
Josh Thao <joshuanhiathao@gmail.com>
2014-08-02 10:03:21 -0500

    view story

--- WebContent/story/view_story.jsp
+++ WebContent/story/view_story.jsp
@@ -1,12 +1,54 @@
-<%@ page language="java" contentType="text/html; charset=ISO-8859-1"
-    pageEncoding="ISO-8859-1"%>
+<%@ page language="java" contentType="text/html; charset=US-ASCII"
+  pageEncoding="US-ASCII"%>
 <!DOCTYPE html PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN" "http://www.w3.org/TR/html4/loose.dtd">
 <html>
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=ISO-8859-1">
-<title>Insert title here</title>
+<meta http-equiv="Content-Type" content="text/html; charset=US-ASCII">
+<title>Tasks</title>
+<%@ include file="../partials/include_bootstrap_partial.jsp"%>
 </head>
-<body>
+<body role="document">
+  <%@ taglib prefix="c" uri="http://java.sun.com/jsp/jstl/core" %>
+  <%@ include file="../partials/navigation_bar_partial.jsp"%>
 
+  <div id="main" class="container theme-showcase" role="main">
+    <div id="success"></div>
+    <p>
+    <a href="new"><button class="btn btn-primary">New Task</button></a><br />
+    </p>
+    <div class="span12"><hr /></div>
+    <div id="task-table" class="span12">
+    <table id="task-table" class="table table-condensed table-hover">
+      <thead>
+      <tr>
+        <th>ID</th>
+        <th>Name</th>
+        <th>Description</th>
+        <th>Assigned To</th>
+        <th>Story</th>
+        <th>Status</th>
+        <th><!-- Button column --></th>
+      <tr>
+      </thead>
+      <c:forEach var="t" items="${task_list}">
+        <tr>
+          <td>${t.taskId}</td>          
+          <td>${t.name}</td>          
+          <td>${t.description}</td>          
+          <td>${t.assignedToUser.username}</td>          
+          <td>${t.story.storyName}</td>
+          <td>${t.status}</td>
+          <td>
+            <a href="../task/view?task_id=${t.taskId}">
+                <button class="btn btn-primary">View</button>
+            </a>
+          </td>
+        </tr>
+      </c:forEach>
+    </table>
+    </div>
+  </div>
+
+  <%@ include file="../partials/include_bootstrap_javascript.jsp"%>
 </body>
 </html>
--- src/com/scrumware/story/ViewStoryServlet.java
+++ src/com/scrumware/story/ViewStoryServlet.java
@@ -10,7 +10,13 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
+import org.json.JSONArray;
+import org.json.JSONObject;
+
 import com.scrumware.config.Constants;
+import com.scrumware.task.DetailedTask;
+import com.scrumware.task.Task;
+import com.scrumware.task.TaskDB;
 import com.scrumware.user.User;
 import com.scrumware.user.UserDB;
 
@@ -22,24 +28,70 @@ import com.scrumware.user.UserDB;
 public class ViewStoryServlet extends HttpServlet {
 	
 	
+	private static final long serialVersionUID = 1L;
+
+	private static final String DATA_TYPE_PARAMETER = "data_type";
+       
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public ViewStoryServlet() {
+        super();
+    }
+
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-	
 		if (!isValidSession(request)) {
 			response.sendRedirect(request.getContextPath() + "/login.jsp");
 			return;
 		}
 		
+		String dataType = request.getParameter(DATA_TYPE_PARAMETER);
+		String userId = request.getParameter(Constants.USER_ID);
+		String storyId = request.getParameter(Constants.STORY_ID);
+		ArrayList<Task> taskList;
+
+		if ("json".equals(dataType) && storyId != null) {
+			taskList = TaskDB.getAllTasksForStory(Integer.parseInt(storyId));
+			
+			JSONObject jsonObject = createJSONObject(taskList);
+			response.addHeader("Content-Type", "application/json");
+			response.getWriter().println(jsonObject);
+		} else {
+			taskList = TaskDB.getAllTasksForStory(Integer.parseInt(storyId));
+			ArrayList<DetailedTask> taskDetailList = new ArrayList<DetailedTask>();
+			
+			for (Task task : taskList) {
+				User user = UserDB.getUser(task.getAssignedTo());
+				Story story = StoryDB.getStory(task.getStoryId());
+				DetailedTask detailedTask = new DetailedTask(task);
+				detailedTask.setAssignedTo(user);
+				detailedTask.setStory(story);
+				taskDetailList.add(detailedTask);
+			}
+			
+			request.setAttribute("task_list", taskDetailList);
+			request.getRequestDispatcher("/story/view_story.jsp").forward(request, response);
+		}
 	}
 	
-
 	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		doGet(request, response);
+		doGet(request,response);
+	}
+	
+	private JSONObject createJSONObject(ArrayList<Task> taskList) {
+		JSONObject jsonObject = new JSONObject();
+		JSONArray jsonArray = new JSONArray();
+		for (Task task : taskList) {
+			jsonArray.put(task.toJSON());
+		}
+		jsonObject.put("result", jsonArray);
+		return jsonObject;
 	}
 	
 	private boolean isValidSession(HttpServletRequest request) {
@@ -58,5 +110,4 @@ public class ViewStoryServlet extends HttpServlet {
 			return true;
 		}
 	}
-
 }
